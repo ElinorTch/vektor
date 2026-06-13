ca4fe01dc087a9d34dea9fc73c5f53f76056c0de
Elinor Tchamo <tchamoelii@gmail.com>
2026-06-13 20:35:49 +0200

    Feat: api initialization

---
+++ vektor-api/src/main/java/com/codewitheli/vektorapi/VektorApiApplication.java
@@ -0,0 +1,13 @@
+package com.codewitheli.vektorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VektorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VektorApiApplication.class, args);
+    }
+
+}
---
+++ vektor-api/src/test/java/com/codewitheli/vektorapi/VektorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewitheli.vektorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VektorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
